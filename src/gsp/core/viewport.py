f0af6a7954c4b17fa764fcea2f5bc2e7e6abbddd
Jerome Etienne <jerome.etienne@gmail.com>
2025-12-03 16:06:17 +0100

    Enhance docstrings in Viewport class: add parameter and return descriptions for clarity

--- src/gsp/core/viewport.py
+++ src/gsp/core/viewport.py
@@ -9,6 +9,18 @@ class Viewport:
     __slots__ = ["_uuid", "_x", "_y", "_width", "_height", "userData"]
 
     def __init__(self, x: int, y: int, width: int, height: int):
+        """
+        Create a viewport.
+
+        :param x: offset from left in pixels
+        :type x: int
+        :param y: offset from bottom in pixels
+        :type y: int
+        :param width: width in pixels
+        :type width: int
+        :param height: height in pixels
+        :type height: int
+        """
         self._uuid: str = UuidUtils.generate_uuid()
         self._x: int = x
         self._y: int = y
@@ -23,13 +35,25 @@ class Viewport:
         return self._uuid
 
     def get_x(self) -> int:
+        """
+        Get the x offset (in pixels from left) of the viewport.
+        """
         return self._x
 
     def get_y(self) -> int:
+        """
+        Get the y offset (in pixels from bottom) of the viewport.
+        """
         return self._y
 
     def get_width(self) -> int:
+        """
+        Get the width (in pixels) of the viewport.
+        """
         return self._width
 
     def get_height(self) -> int:
+        """
+        Get the height (in pixels) of the viewport.
+        """
         return self._height
