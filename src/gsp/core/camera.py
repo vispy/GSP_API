f2bfc78eb7c45378866210130f586761a99b36fe
Jerome Etienne <jerome.etienne@gmail.com>
2025-12-11 18:21:45 +0100

    Update docstrings

--- src/gsp/core/camera.py
+++ src/gsp/core/camera.py
@@ -10,25 +10,57 @@ class Camera:
     __slots__ = ["_uuid", "_view_matrix", "_projection_matrix", "userData"]
 
     def __init__(self, view_matrix: TransBuf, projection_matrix: TransBuf):
+        """Initialize a Camera instance. Just a container for view and projection matrices.
+
+        Args:
+            view_matrix (TransBuf): View matrix - [view-matrix](https://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/#the-view-matrix)
+            projection_matrix (TransBuf): Projection matrix - [projection-matrix](https://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/#the-projection-matrix)
+        """
         self._uuid: str = UuidUtils.generate_uuid()
         self._view_matrix: TransBuf = view_matrix
         self._projection_matrix: TransBuf = projection_matrix
         self.userData: dict[str, Any] = {}
+        """A dictionary for storing custom user data associated with the Camera instance."""
 
     def __repr__(self) -> str:
         return f"Camera(uuid={self._uuid})"
 
     def get_uuid(self) -> str:
+        """Get the UUID of the Camera instance.
+
+        Returns:
+            str: The UUID of the Camera.
+        """
         return self._uuid
 
     def set_view_matrix(self, view_matrix: TransBuf):
+        """Set the view matrix of the Camera.
+
+        Args:
+            view_matrix (TransBuf): The new view matrix.
+        """
         self._view_matrix = view_matrix
 
     def get_view_matrix(self) -> TransBuf:
+        """Get the view matrix of the Camera.
+
+        Returns:
+            TransBuf: The view matrix.
+        """
         return self._view_matrix
 
     def set_projection_matrix(self, projection_matrix: TransBuf):
+        """Set the projection matrix of the Camera.
+
+        Args:
+            projection_matrix (TransBuf): The new projection matrix.
+        """
         self._projection_matrix = projection_matrix
 
     def get_projection_matrix(self) -> TransBuf:
+        """Get the projection matrix of the Camera.
+
+        Returns:
+            TransBuf: The projection matrix.
+        """
         return self._projection_matrix
