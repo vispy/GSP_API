9c0e203a0657e0c4dfd7a2465c8e82b5bae523b1
Jerome Etienne <jerome.etienne@gmail.com>
2026-03-22 20:08:31 +0100

    Add Mesh rendering support to MatplotlibRenderer and implement RendererMeshBasicMaterial

---
+++ src/gsp_matplotlib/renderer/matplotlib_renderer_mesh.py
@@ -0,0 +1,146 @@
+# stdlib imports
+import typing
+
+# pip imports
+import matplotlib.artist
+import numpy as np
+
+
+# local imports
+from gsp.constants import Constants
+from gsp.visuals.mesh import Mesh
+from gsp.materials import MeshBasicMaterial, MeshPhongMaterial, MeshNormalMaterial, MeshDepthMaterial, MeshTexturedMaterial
+from .matplotlib_renderer import MatplotlibRenderer
+from gsp.core.camera import Camera
+from gsp.core.viewport import Viewport
+from gsp.types.transbuf import TransBuf
+
+# from ..cameras.camera import Camera
+# from ..math.transform_utils import TransformUtils
+from gsp.geometry.geometry_utils import GeometryUtils
+
+# https://chatgpt.com/c/68ee0eab-776c-8331-b44a-f131ba3f166b
+# local -> world -> view -> clip (NDC) -> screen (2D)
+
+# local -> world : coord in world space
+# world -> view : coord in camera space
+# view -> clip (NDC) : coord in normalized device coord space
+# clip (NDC) -> screen (2D) : coord in 2D screen
+
+
+class RendererMesh:
+    """Renderer for Mesh visual using Matplotlib."""
+
+    @staticmethod
+    def render(
+        renderer: MatplotlibRenderer,
+        viewport: Viewport,
+        mesh: Mesh,
+        model_matrix: TransBuf,
+        camera: Camera,
+    ) -> list[matplotlib.artist.Artist]:
+
+        # =============================================================================
+        # sanity checks
+        # =============================================================================
+
+        assert mesh.geometry.indices is not None, "The mesh geometry must have face indices to be rendered"
+        assert mesh.geometry.uvs is not None, "The mesh geometry must have texture coordinates to be rendered"
+
+        # =============================================================================
+        # Extract geometry and material
+        # =============================================================================
+
+        geometry = mesh.geometry
+        material = mesh.material
+        faces_uvs = mesh.geometry.uvs[geometry.indices]
+
+        # =============================================================================
+        # Compute the world space faces_vertices
+        # =============================================================================
+
+        # Get the full transform matrix for the mesh
+        world_matrix = mesh.get_world_matrix()
+        vertices_world = GeometryUtils.apply_transform(geometry.vertices, world_matrix)
+
+        # build the faces vertices and uvs arrays
+        faces_vertices_world = vertices_world[geometry.indices]
+
+        # =============================================================================
+        # Compute the NDC faces_vertices
+        # =============================================================================
+
+        # Get the full transform matrix for the mesh
+        mvp_matrix = TransformUtils.compute_mvp_matrix(camera, mesh)
+        vertices_ndc, vertices_clip = GeometryUtils.apply_mvp_matrix(geometry.vertices, mvp_matrix)
+
+        # build the faces vertices and uvs arrays
+        faces_vertices_ndc = vertices_ndc[geometry.indices]
+
+        # =============================================================================
+        # Switch vertices to 2d
+        # =============================================================================
+
+        # drop z for 2D rendering
+        faces_vertices_2d = faces_vertices_ndc[..., :2]
+
+        # =============================================================================
+        # Render the mesh using the appropriate material
+        # =============================================================================
+
+        if isinstance(mesh.material, MeshBasicMaterial):
+            from .matplotlib_renderer_mesh_basic import RendererMeshBasicMaterial
+
+            changed_artists = RendererMeshBasicMaterial.render(
+                renderer=renderer,
+                mesh=mesh,
+                camera=camera,
+                faces_vertices_ndc=faces_vertices_ndc,
+                faces_vertices_2d=faces_vertices_2d,
+            )
+        # elif isinstance(mesh.material, MeshNormalMaterial):
+        #     from .renderer_mesh_normal_material import RendererMeshNormalMaterial
+
+        #     changed_artists = RendererMeshNormalMaterial.render(
+        #         renderer=renderer,
+        #         mesh=mesh,
+        #         camera=camera,
+        #         faces_vertices_world=faces_vertices_world,
+        #         faces_vertices_ndc=faces_vertices_ndc,
+        #         faces_vertices_2d=faces_vertices_2d,
+        #     )
+        # elif isinstance(mesh.material, MeshDepthMaterial):
+        #     from .renderer_mesh_depth_material import RendererMeshDepthMaterial
+
+        #     changed_artists = RendererMeshDepthMaterial.render(
+        #         renderer=renderer,
+        #         mesh=mesh,
+        #         camera=camera,
+        #         faces_vertices_ndc=faces_vertices_ndc,
+        #         faces_vertices_2d=faces_vertices_2d,
+        #     )
+        # elif isinstance(mesh.material, MeshPhongMaterial):
+        #     from .renderer_mesh_phong_material import RendererMeshPhongMaterial
+
+        #     changed_artists = RendererMeshPhongMaterial.render(
+        #         renderer=renderer,
+        #         camera=camera,
+        #         mesh=mesh,
+        #         faces_vertices_world=faces_vertices_world,
+        #         faces_vertices_ndc=faces_vertices_ndc,
+        #         faces_vertices_2d=faces_vertices_2d,
+        #     )
+        # elif isinstance(mesh.material, MeshTexturedMaterial):
+        #     from .renderer_mesh_textured_material import RendererMeshTexturedMaterial
+
+        #     changed_artists = RendererMeshTexturedMaterial.render(
+        #         renderer=renderer,
+        #         mesh=mesh,
+        #         camera=camera,
+        #         faces_vertices_world=faces_vertices_world,
+        #         faces_vertices_2d=faces_vertices_2d,
+        #         faces_uvs=faces_uvs,
+        #     )
+        else:
+            raise ValueError(f"Unsupported material type: {type(mesh.material)}")
+        return changed_artists
---
+++ src/gsp_matplotlib/renderer/matplotlib_renderer_mesh_basic.py
@@ -0,0 +1,118 @@
+# stdlib imports
+import typing
+
+# pip imports
+import matplotlib.artist
+import matplotlib.collections
+import numpy as np
+
+
+# local imports
+from gsp.materials import MeshBasicMaterial
+from gsp.visuals.mesh import Mesh
+from .matplotlib_renderer import MatplotlibRenderer
+from gsp.core.camera import Camera
+from .renderer_utils import RendererUtils
+
+
+class RendererMeshBasicMaterial:
+    """A class responsible for rendering Mesh visuals with MeshBasicMaterial using Matplotlib."""
+
+    @staticmethod
+    def render(
+        renderer: MatplotlibRenderer,
+        viewport: Viewport,
+        mesh: Mesh,
+        camera: Camera,
+        faces_vertices_ndc: np.ndarray,
+        faces_vertices_2d: np.ndarray,
+    ) -> list[matplotlib.artist.Artist]:
+        geometry = mesh.geometry
+        material = typing.cast(MeshBasicMaterial, mesh.material)
+
+        # =============================================================================
+        # Sanity checks
+        # =============================================================================
+
+        # faces_vertices_ndc: (n_faces, 3, 3) array of the 3D vertices of each face in NDC space
+        # faces_vertices_2d: (n_faces, 3, 2) array of the 2D vertices of each face in screen space
+        # faces_uvs: (n_faces, 3, 2) array of the UV coordinates of each face
+
+        assert isinstance(material, MeshBasicMaterial), f"Expected material to be a MeshBasicMaterial, got {type(material)}"
+        assert faces_vertices_ndc.shape == (
+            len(geometry.indices),
+            3,
+            3,
+        ), f"Expected faces_vertices_ndc to have shape {(len(geometry.indices), 3, 3)}, got {faces_vertices_ndc.shape}"
+        assert faces_vertices_2d.shape == (
+            len(geometry.indices),
+            3,
+            2,
+        ), f"Expected faces_vertices_2d to have shape {(len(geometry.indices), 3, 2)}, got {faces_vertices_2d.shape}"
+        assert len(faces_vertices_2d) == len(
+            geometry.indices
+        ), f"Expected faces_vertices_2d to have {len(geometry.indices)} faces, got {len(faces_vertices_2d)}"
+
+        # =============================================================================
+        # Honor material.face_sorting
+        # =============================================================================
+
+        # Sort polygons by depth (painter's algorithm)
+        # - faces are sorted based on their depth (z) in camera space within a single artist
+        # - this artist.set_zorder() is set based on the distance from the camera to the Object3D position
+        # - so possible conflict between faces of different objects
+        # - CAUTION: here reorder ALL arrays you use below to keep them in sync
+        if material.face_sorting:
+            # compute the depth of each face as the mean z value of its vertices
+            faces_depth = faces_vertices_ndc[:, :, 2].mean(axis=1)
+            # get the sorting indices (from farthest to nearest)
+            depth_sorted_indices = np.argsort(faces_depth)
+            # apply the sorting to faces_vertices and faces_hidden
+            # CAUTION: here reorder ALL arrays you use below to keep them in sync
+            faces_vertices_2d = faces_vertices_2d[depth_sorted_indices]
+
+        # =============================================================================
+        # honor material.face_culling
+        # =============================================================================
+
+        faces_visible = RendererUtils.compute_faces_visible(faces_vertices_2d, material.face_culling)
+        # print(f"faces_visible: {faces_visible.sum()}/{len(faces_visible)}")
+
+        # remove hidden faces
+        faces_vertices_2d = faces_vertices_2d[faces_visible]
+
+        # =============================================================================
+        # Create artists if needed
+        # =============================================================================
+        if mesh.get_uuid() not in renderer._artists:
+            axes = renderer.get_mpl_axes_for_viewport(viewport)
+            mpl_poly_collection = matplotlib.collections.PolyCollection([], clip_on=False, snap=False)
+            mpl_poly_collection.set_visible(False)  # hide until properly positioned and sized
+            axes.add_collection(mpl_poly_collection)
+            renderer._artists[mesh.get_uuid()] = mpl_poly_collection
+
+        # =============================================================================
+        # Get the mpl_artist
+        # =============================================================================
+
+        mpl_poly_collection = typing.cast(matplotlib.collections.PolyCollection, renderer._artists[mesh.get_uuid()])
+        mpl_poly_collection.set_visible(True)
+
+        # =============================================================================
+        # do z-ordering based on distance to camera
+        # =============================================================================
+
+        # # compute and set zorder on our single artist
+        # RendererUtils.update_single_artist_zorder(camera, mesh, mpl_poly_collection)
+
+        # =============================================================================
+        # Update all the artists
+        # =============================================================================
+
+        # update the PathCollection with the new patches
+        mpl_poly_collection.set_verts(typing.cast(list, faces_vertices_2d))
+        mpl_poly_collection.set_facecolor(typing.cast(list, material.colors))
+        mpl_poly_collection.set_edgecolor(typing.cast(list, material.edge_colors))
+        mpl_poly_collection.set_linewidth(typing.cast(list, material.edge_widths))
+
+        return [mpl_poly_collection]
